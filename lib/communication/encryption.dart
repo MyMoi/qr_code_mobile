2dd2e083ca571881b7bfe47b01798a750166253b
Pasdesushi <titolivier@gmx.fr>
2020-12-10 18:51:03 +0100

    first fonctionnal version

--- lib/communication/encryption.dart
+++ lib/communication/encryption.dart
@@ -8,15 +8,19 @@ void main() {
 */
 String decrypt(String content, String iv, String key) {
   final encrypter = Encrypter(AES(Key.fromBase64(key), mode: AESMode.cbc));
-  return encrypter.decrypt(Encrypted.fromBase16(content),
-      iv: IV.fromBase16(iv));
+  return encrypter.decrypt(Encrypted.fromBase64(content),
+      iv: IV.fromBase64(iv));
 }
 
 encrypt(String content, String key) {
   final iv = IV.fromSecureRandom(16);
-  final encrypter = Encrypter(AES(Key.fromBase64(key), mode: AESMode.cbc));
+  final Key _key = Key.fromBase64(key);
+  print(_key.length);
+  print(_key.bytes);
+  final encrypter = Encrypter(AES(_key, mode: AESMode.cbc));
+
   return {
-    'content': encrypter.encrypt(content, iv: iv).base16,
-    'iv': iv.base16
+    'content': encrypter.encrypt(content, iv: iv).base64,
+    'iv': iv.base64
   };
 }
